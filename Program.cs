02f8540eea32aa8842e493cf1a8b8025b8576630
Wendell Mickael Cardoso <135742936+Oiquerida69@users.noreply.github.com>
2023-06-15 16:13:17 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,22 +9,26 @@ namespace ATV07_JOGOS
 {
     class Program
     {
+
         private static int resultado;
         private static int resultado2;
         private static int resultado3;
         private static int resultado4;
-        public static void Main (string[] args)
+
+
+        public static void Main(string[] args)
         {
             Random sorteador = new Random();
-            Random sorteador2 = new Random();
-            Random sorteador3 = new Random();
-            Random sorteador4 = new Random();
-            Random sorteador5 = new Random();
+
+
+
+
             int sorteio = sorteador.Next(1, 11);
-            int sorteio2 = sorteador2.Next(1, 11);
-            int sorteio3 = sorteador3.Next(1, 11);
-            int sorteio4 = sorteador4.Next(1, 11);
-            int sorteio5 = sorteador5.Next(1, 11);
+
+
+
+
+
 
 
 
@@ -35,6 +39,8 @@ namespace ATV07_JOGOS
             int vencedor = 21;
             if (resposta == "sim")
             {
+                Random sorteador2 = new Random();
+                int sorteio2 = sorteador2.Next(1, 11);
                 resultado = sorteio + sorteio2;
                 Console.WriteLine(resultado);
             }
@@ -44,58 +50,90 @@ namespace ATV07_JOGOS
             }
             Console.WriteLine("você deseja mais um numero:");
             Console.Write("- Sim");
-            Console.Write("- Não");
+            Console.WriteLine("- Não");
             string resposta2 = Console.ReadLine();
+            if (resultado > 21)
+            {
+                Console.WriteLine("você estorou e infelizmente perdeu ;[");
+                return;
+            }
             if (resposta2 == "sim")
             {
+                Random sorteador3 = new Random();
+                int sorteio3 = sorteador3.Next(1, 11);
                 resultado2 = resultado + sorteio3;
                 Console.WriteLine(resultado2);
+            }
+            if (resultado2 == vencedor)
+            {
+                Console.WriteLine("você chegou a vinte um , parabéns você venceu");
+                
             }
             else
             {
                 Console.WriteLine($"você desejou para por aqui o seu valor final é{resultado} ");
-            }
-            if (resultado2 > vencedor)
-            {
-                Console.WriteLine("você estorou e infelimente perdeu ;[");
+                Console.Clear();
                 return;
-
             }
+
 
 
 
             Console.WriteLine("você deseja mais um numero:");
             Console.Write("- Sim");
-            Console.Write("- Não");
+            Console.WriteLine("- Não");
             string resposta3 = Console.ReadLine();
-            if (vencedor == resultado2)
+            if (resultado2 > vencedor)
+            {
+                Console.WriteLine("você estorou e infelizmente perdeu ;[");
+                return;
+            }
+            if (resultado2 == vencedor)
             {
                 Console.WriteLine("Parabéns você ganhou o jogo");
+               
             }
-            else if (resposta3 == "sim")
+            if (resposta3 == "sim")
             {
+                Random sorteador4 = new Random();
+                int sorteio4 = sorteador4.Next(1, 11);
                 resultado3 = resultado2 + sorteio4;
                 Console.WriteLine(resultado3);
             }
             else
             {
                 Console.WriteLine($"você desejou parar por aqui o seu valor final é {resultado2} ");
+                Console.Clear();
             }
-            if (vencedor == resultado3)
+            if (resultado3 > 21)
+            {
+                Console.WriteLine("você estorou e infelizmente perdeu ;[");
+                return;
+            }
+            if (resultado3 == vencedor)
             {
                 Console.WriteLine("você chegou a vinte um , parabéns você venceu");
-                return;
+                
 
             }
             Console.WriteLine($"o seu resultado agora é{resultado3}, você deseja mais um numero ?");
             Console.Write("- sim");
-            Console.Write("- não");
+            Console.WriteLine("- não");
             string resposta5 = Console.ReadLine();
             if (resposta5 == "sim")
             {
+                Random sorteador5 = new Random();
+                int sorteio5 = sorteador5.Next(1, 11);
                 resultado4 = resultado3 + sorteio5;
                 Console.WriteLine(resultado4);
             }
+            if (resultado > 21)
+            {
+                Console.WriteLine("você estorou e infelizmente perdeu ;[");
+
+                return;
+            }
+
             if (resultado4 == vencedor)
             {
                 Console.WriteLine("parabèns você venceu");
@@ -103,11 +141,16 @@ namespace ATV07_JOGOS
             else
             {
                 Console.WriteLine($"você desejou parar por aqui o seu resultado final é{resultado4} ");
-                
+                Console.Clear();
+
             }
             Console.ReadLine();
         }
     }
 }
+            
+        
     
+
+
 
